a9d3fb2de173d46eeb4b62b7fd47b5c01529f501
jyrkim <jyrki.mizaras@gmail.com>
2015-02-23 22:59:32 +0200

    config.assets.compile = true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,8 @@ FlapperNews::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true  #original false
+  #http://stackoverflow.com/questions/20588786/glyphicon-works-locally-but-not-on-heroku
 
   # Generate digests for assets URLs.
   config.assets.digest = true
